181f7693cf4ecc56fda90bcb4c0f0ad3e35f1482
saketkumar1996 <callmesaket@gmail.com>
2022-12-10 02:02:06 +0530

    method in user's schema for token

--- models/user.schema.js
+++ models/user.schema.js
@@ -3,6 +3,7 @@ import authRoles from "../utils/authRoles";
 import bcrypt from 'bcryptjs';
 import JWT from 'jsonwebtoken';
 import crypto from 'crypto'
+import config from "../config";
 
 
 const userSchema = mongoose.Schema(
@@ -42,5 +43,28 @@ userSchema.pre('save', async function(next){
     this.password = await bcrypt.hash(this.password,10);
     next()
 })
+
+// compare password
+userSchema.methods = {
+    comparePassword: async function(enteredPassword){
+        return await bcrypt.compare(enteredPassword,this.password)
+    },
+    
+    getJwtToken: function(){
+        return JWT.sign(
+          {
+            _id: this._id,
+            role:this.role
+          },
+          config.JWT_SECRET,
+          {
+            expiresIn: config.JWT_EXPIRY
+          }  
+        )
+    }
+}
+
+
+
 
 export default mongoose.model("User",userSchema)
